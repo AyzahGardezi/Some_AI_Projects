fcbc50d77a0e1a08f0665132976bffdc7b9ad608
Ayzah Gardezi <116636136+AyzahGardezi@users.noreply.github.com>
2026-06-04 20:58:54 +0500

    Changing LLM from OpenAI to llama

--- AI_Chatbot/main.py
+++ AI_Chatbot/main.py
@@ -1,20 +1,20 @@
-from langchain_core.messages import HumanMessage    # high level framework that allows us to build AI applications
-from langchain_openai import ChatOpenAI             # allow us to use open AI within LangChain and LangGraph
-from langchain.tools import tool
-from langgraph.prebuilt import create_react_agent   # complex framework that allows us to build AI agents
+from langchain_core.messages import HumanMessage
+from langchain_groq import ChatGroq
+from langgraph.prebuilt import create_react_agent
 from dotenv import load_dotenv
-
-# AI agent has access to tools - tool can be anything, calculate, logical operations, etc
 
 load_dotenv()
 
 def main():
-    model = ChatOpenAI(temperature=0)   # low randomness of the model
+    model = ChatGroq(
+        model="llama-3.3-70b-versatile",
+        temperature=0
+    )
 
     tools = []
     agent_executor = create_react_agent(model, tools)
 
-    print("AI assistant lauched. Type 'quit' to exit.")
+    print("AI assistant launched. Type 'quit' to exit.")
     print("I can perform calculations and chat with you.")
 
     while True:
@@ -23,11 +23,8 @@ def main():
         if user_input == "quit":
             break
 
-        print("\nAssistant: ", end="") # end stops new line to be added
+        print("\nAssistant: ", end="")
 
-        # chunks are parts of a response coming from the agent
-        # loop through the chunks to see if there is a response from the agent
-        # if there is a response then we see if there are any messages in the response
         for chunk in agent_executor.stream(
             {"messages": [HumanMessage(content=user_input)]}
         ):
@@ -39,4 +36,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
